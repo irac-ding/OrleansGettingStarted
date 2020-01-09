32ff095a2325d167f0fd573e5a6cf105298eba1a
irac <iracding@tvunetworks.com>
2020-01-09 18:18:36 +0800

    update

--- src/Kritner.Orleans.GettingStarted.Grains/EverythingIsOkGrain.cs
+++ src/Kritner.Orleans.GettingStarted.Grains/EverythingIsOkGrain.cs
@@ -28,8 +28,21 @@ namespace Kritner.Orleans.GettingStarted.Grains
                     "maggie@anykey.com"
                 },
                 "Everything's ok!",
-                "This alarm will sound every 1 minute, as long as everything is ok!"
+                $"This is from reminder:{this.GetPrimaryKeyString()}, This alarm will sound every 1 minute, as long as everything is ok!"
             );
+            ////when ReceiveReminder we kppp activ the gain and update the reminder to break the period at least 1 minute limit.
+            //string keyGain = $"{nameof(IEverythingIsOkGrain)}-{Guid.NewGuid()}";
+            //var grain = GrainFactory.GetGrain<IEverythingIsOkGrain>(keyGain);
+            //await RegisterOrUpdateReminder(
+            //    keyGain,
+            //    TimeSpan.FromSeconds(3),
+            //    TimeSpan.FromMinutes(1) // apparently the minimum
+            //);
+
+            //when ReceiveReminder we kppp active a new gain and register the reminder.
+            string keyGain = $"{nameof(IEverythingIsOkGrain)}-{Guid.NewGuid()}";
+            var grain = GrainFactory.GetGrain<IEverythingIsOkGrain>(keyGain);
+            await grain.Start();
         }
 
         public async Task Start()
@@ -44,8 +57,25 @@ namespace Kritner.Orleans.GettingStarted.Grains
                 TimeSpan.FromSeconds(3),
                 TimeSpan.FromMinutes(1) // apparently the minimum
             );
+            //RegisterTimer((obj)=>
+            //{
+            //    var emailSenderGrain = GrainFactory.GetGrain<IEmailSenderGrain>(Guid.Empty);
+            //    emailSenderGrain.SendEmail(
+            //        "homer@anykey.com",
+            //        new[]
+            //        {
+            //        "marge@anykey.com",
+            //        "bart@anykey.com",
+            //        "lisa@anykey.com",
+            //        "maggie@anykey.com"
+            //        },
+            //        "Everything's ok!",
+            //        "This is from timer, This alarm will sound every 1 second, as long as everything is ok!"
+            //    );
+            //    return Task.CompletedTask;
+            //}, this.GetPrimaryKeyString(), TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(1));
         }
-
+        
         public async Task Stop()
         {
             await UnregisterReminder(_reminder);
