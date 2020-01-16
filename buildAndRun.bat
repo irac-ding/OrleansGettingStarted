e85d2f56d2f2c7a4e4164ed56c89e1120316c610
irac <iracding@tvunetworks.com>
2020-01-16 17:11:17 +0800

    update

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -1,9 +1,6 @@
 cd /d %~dp0
 start /wait  build.bat
 echo "build done"
-start  consulRun.bat
-TIMEOUT /T 5
 start  runServer.bat
-TIMEOUT /T 5
+TIMEOUT /T 10
 start  runClient.bat
-start "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8500/ui
