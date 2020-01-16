f4f850b04f4b3f3373021d1738d069fac99dac30
irac <iracding@tvunetworks.com>
2020-01-16 16:36:37 +0800

    update

--- consulRun.sh
+++ consulRun.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
+basepath=$(cd `dirname $0`; pwd)
 consul agent -ui -server -bootstrap -data-dir "/data/Consul/Data" -client=0.0.0.0
--- runClient.sh
+++ runClient.sh
@@ -1,1 +1,3 @@
+#/bin/bash
+basepath=$(cd `dirname $0`; pwd)
 dotnet run --project ./src/Kritner.OrleansGettingStarted.Client --no-build 
