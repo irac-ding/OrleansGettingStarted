ccaa009d6e21f6facfc900ad16c8db5b52867e06
irac <iracding@tvunetworks.com>
2020-01-16 16:13:11 +0800

    update

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -3,6 +3,8 @@ basepath=$(cd `dirname $0`; pwd)
 output=`bash build.sh`
 # bash consulRun.sh
 sleep 5s
+chmod 755 runServer.sh
 exec ./runServer.sh
 sleep 5s
+chmod 755 runServer.sh
 exec ./runClient.sh
