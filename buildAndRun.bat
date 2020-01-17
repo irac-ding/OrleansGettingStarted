59c76d9b136cfc46745cae29a0c4f83d6d451b3f
irac-ding <54928255+irac-ding@users.noreply.github.com>
2020-01-17 16:02:31 +0800

    Update buildAndRun.bat

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -4,3 +4,5 @@ echo "build done"
 start  runServer.bat
 TIMEOUT /T 10
 start  runClient.bat
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8500/ui
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8080/
